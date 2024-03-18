7ff67a782c6a480422819d242f7ae2bf6512e4d6
Din Music <din.music@canonical.com>
2024-03-18 14:15:46 +0100

    simplestream-maintainer/cmd_prune: Add option for stream version to prune commands
    Signed-off-by: Din Music <din.music@canonical.com>

--- simplestream-maintainer/cmd_prune.go
+++ simplestream-maintainer/cmd_prune.go
@@ -14,9 +14,10 @@ import (
 )
 
 type DiscardOptions struct {
-	Dangling  bool
-	RetainNum int
-	ImageDirs []string
+	Dangling      bool
+	RetainNum     int
+	StreamVersion string
+	ImageDirs     []string
 }
 
 func NewDiscardCmd() *cobra.Command {
@@ -32,8 +33,9 @@ func NewDiscardCmd() *cobra.Command {
 		},
 	}
 
-	cmd.PersistentFlags().BoolVar(&o.Dangling, "dangling", false, "Remove dangling product versions (not referenced from product catalog)")
+	cmd.PersistentFlags().BoolVar(&o.Dangling, "dangling", false, "Remove dangling product versions (not referenced from any product catalog)")
 	cmd.PersistentFlags().IntVar(&o.RetainNum, "retain", 10, "Number of product versions to retain")
+	cmd.PersistentFlags().StringVar(&o.StreamVersion, "stream-version", "v1", "Stream version")
 	cmd.PersistentFlags().StringSliceVarP(&o.ImageDirs, "image-dir", "d", []string{"images"}, "Image directory (relative to path argument)")
 
 	return cmd
@@ -41,22 +43,18 @@ func NewDiscardCmd() *cobra.Command {
 
 func (o *DiscardOptions) Run(args []string) error {
 	if len(args) < 1 || args[0] == "" {
-		return fmt.Errorf("argument %q is required and cannot be empty", "path")
-	}
-
-	if o.RetainNum < 1 {
-		return fmt.Errorf("at least 1 product version must be retained")
+		return fmt.Errorf("Argument %q is required and cannot be empty", "path")
 	}
 
 	for _, dir := range o.ImageDirs {
 		if o.Dangling {
-			err := pruneDanglingProductVersions(args[0], dir)
+			err := pruneDanglingProductVersions(args[0], o.StreamVersion, dir)
 			if err != nil {
 				return err
 			}
 		}
 
-		err := pruneStreamProductVersions(args[0], dir, o.RetainNum)
+		err := pruneStreamProductVersions(args[0], o.StreamVersion, dir, o.RetainNum)
 		if err != nil {
 			return err
 		}
@@ -65,45 +63,48 @@ func (o *DiscardOptions) Run(args []string) error {
 	return nil
 }
 
-// pruneStreamProductVersions reads contents of index.json, finds the existing streams,
-// and prunes the old-enough product versions.
-func pruneStreamProductVersions(rootDir string, streamName string, retain int) error {
-	jsonPath := filepath.Join(rootDir, "streams", "v1", fmt.Sprintf("%s.json", streamName))
+// pruneStreamProductVersions reads the product catalog and removes all product
+// versions except for the number of latests versions defined by retain integer.
+func pruneStreamProductVersions(rootDir string, streamVersion string, streamName string, retain int) error {
+	if retain < 1 {
+		return fmt.Errorf("At least 1 product version must be retained")
+	}
 
-	stream, err := shared.ReadJSONFile(jsonPath, &stream.ProductCatalog{})
+	// Read product catalog.
+	catalogPath := filepath.Join(rootDir, "streams", streamVersion, fmt.Sprintf("%s.json", streamName))
+	catalog, err := shared.ReadJSONFile(catalogPath, &stream.ProductCatalog{})
 	if err != nil {
 		return err
 	}
 
-	// Find and store versions that need to be discarded.
-	// First, we need to update stream's json file to ensure users do not
-	// try to download non-existing images.
+	// Find versions that need to be discarded.
 	var discardVersions []string
 
-	for i, p := range stream.Products {
+	for i, p := range catalog.Products {
 		productPath := filepath.Join(rootDir, streamName, p.RelPath())
 
 		versions := shared.MapKeys(p.Versions)
 		slices.Sort(versions)
+		slices.Reverse(versions)
 
 		if len(versions) <= retain {
+			// All product versions must be retained.
 			continue
 		}
 
 		// Extract versions that need to be discarded.
 		discard := slices.Delete(versions, 0, retain)
 		for _, v := range discard {
-			fmt.Printf("Discarding product version %q\n", fmt.Sprintf("%v[%s]", p.ID(), v))
-
-			delete(stream.Products[i].Versions, v)
+			delete(catalog.Products[i].Versions, v)
 
 			versionPath := filepath.Join(productPath, v)
 			discardVersions = append(discardVersions, versionPath)
 		}
 	}
 
-	// Write new stream to a temporary json file.
-	tmpFile, err := shared.WriteJSONTempFile(stream)
+	// Update catalog removing existing versions to ensure
+	// a non-existing version is never listed for download.
+	tmpFile, err := shared.WriteJSONTempFile(catalog)
 	if err != nil {
 		return err
 	}
@@ -111,20 +112,22 @@ func pruneStreamProductVersions(rootDir string, streamName string, retain int) e
 	defer os.Remove(tmpFile)
 
 	// Replace existing stream json file.
-	err = os.Rename(tmpFile, jsonPath)
+	err = os.Rename(tmpFile, catalogPath)
 	if err != nil {
 		return err
 	}
 
 	// Set read permissions.
-	err = os.Chmod(jsonPath, 0644)
+	err = os.Chmod(catalogPath, 0644)
 	if err != nil {
 		return err
 	}
 
 	// Remove old versions.
-	// TODO: How to handle errors? If image is removed from the index, and removal of the image
-	// fails, the next time we rebuild the index it will be again included in the index.
+	//
+	// TODO: How to handle errors? If an image is removed from the catalog,
+	// but we fail to actually remove it, it will be reincluded in the catalog
+	// next time we rebuild the index.
 	for _, v := range discardVersions {
 		_ = os.RemoveAll(v)
 	}
@@ -133,55 +136,63 @@ func pruneStreamProductVersions(rootDir string, streamName string, retain int) e
 }
 
 // pruneDanglingProductVersions traverses through the stream directory structure
-// and prunes the dangling product versions.
-func pruneDanglingProductVersions(rootDir string, streamName string) error {
-	// Get existing products (from actual directory hierarchy).
-	existingProducts, err := stream.GetProducts(rootDir, streamName, false)
+// and prunes the product versions that are not referenced by the corresponding
+// product catalog.
+func pruneDanglingProductVersions(rootDir string, streamVersion string, streamName string) error {
+	// Get raw products (from actual directory hierarchy).
+	products, err := stream.GetProducts(rootDir, streamName, false)
 	if err != nil {
 		return err
 	}
 
 	// Get current products (from stream json file).
-	jsonPath := filepath.Join(rootDir, "streams", "v1", fmt.Sprintf("%s.json", streamName))
-	stream, err := shared.ReadJSONFile(jsonPath, &stream.ProductCatalog{})
+	catalogPath := filepath.Join(rootDir, "streams", streamVersion, fmt.Sprintf("%s.json", streamName))
+	catalog, err := shared.ReadJSONFile(catalogPath, &stream.ProductCatalog{})
 	if err != nil {
 		return err
 	}
 
-	// Remove product versions that are not referenced, but exists.
-	// Note: Only versions that are at least 1 day old are removed to avoid
-	// removing versions that are being currently uploaded.
-	for key, ep := range existingProducts {
-		sp, ok := stream.Products[key]
-		if !ok {
-			// TODO: If product is not found in the stream and is older than 1 day,
-			// remove entire dangling product.
-			continue
+	// removeIfOlder gets info of the file on the given path and removes it
+	// if it's modification time is older then maxAge.
+	removeIfOlder := func(path string, maxAge time.Duration) error {
+		info, err := os.Stat(path)
+		if err != nil {
+			return err
 		}
 
-		productPath := filepath.Join(rootDir, streamName, ep.RelPath())
+		if time.Since(info.ModTime()) > maxAge {
+			_ = os.RemoveAll(path)
+		}
 
-		for epv := range ep.Versions {
-			_, ok := sp.Versions[epv]
-			if ok {
-				fmt.Printf("Version %s[%s] is referenced\n", ep.ID(), epv)
-				// Version is referenced, nothing to do.
-				continue
-			}
+		return nil
+	}
 
-			versionPath := filepath.Join(productPath, epv)
+	for key, rp := range products {
+		productPath := filepath.Join(rootDir, streamName, rp.RelPath())
 
-			info, err := os.Stat(versionPath)
+		cp, ok := catalog.Products[key]
+		if !ok {
+			// Remove unreferenced product if older then 6 hours.
+			err := removeIfOlder(productPath, 6*time.Hour)
 			if err != nil {
 				return err
 			}
+		} else {
+			// Iterate over detected versions and remove unreferenced ones.
+			for rpv := range rp.Versions {
+				_, ok := cp.Versions[rpv]
+				if ok {
+					// Version is referenced, nothing to do.
+					continue
+				}
 
-			// Remove the version only if the difference between the current time
-			// and last modification is more then 1 day, to avoid accidentally
-			// removing version that is just being uploaded.
-			if time.Since(info.ModTime()) > 24*time.Hour {
-				fmt.Printf("Discrading dangling product version: %s[%s]\n", ep.ID(), epv)
-				_ = os.RemoveAll(versionPath)
+				// Remove unreferenced product version if older
+				// then 6 hours.
+				versionPath := filepath.Join(productPath, rpv)
+				err := removeIfOlder(versionPath, 6*time.Hour)
+				if err != nil {
+					return err
+				}
 			}
 		}
 	}
