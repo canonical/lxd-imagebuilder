f27df44bc267cf56329a5fc90f9668de105ae0e5
Din Music <din.music@canonical.com>
2024-03-25 16:33:21 +0100

    shared/util: Remove no-longer needed WriteJSONTempFile function
    Signed-off-by: Din Music <din.music@canonical.com>

--- shared/util.go
+++ shared/util.go
@@ -455,27 +455,6 @@ func ReadJSONFile[T any](path string, obj *T) (*T, error) {
 	return obj, nil
 }
 
-// WriteJSONTempFile encodes the given structure into JSON format and writes it to the
-// a temporary file. It returns either a path to the new file or an error.
-func WriteJSONTempFile(obj any) (string, error) {
-	file, err := os.CreateTemp(os.TempDir(), "simple-stream.*")
-	if err != nil {
-		return "", fmt.Errorf("Failed creating temporary file: %w", err)
-	}
-
-	defer file.Close()
-
-	encoder := json.NewEncoder(file)
-	encoder.SetIndent("", "  ")
-
-	err = encoder.Encode(obj)
-	if err != nil {
-		return "", fmt.Errorf("Error encoding JSON: %w", err)
-	}
-
-	return file.Name(), nil
-}
-
 // WriteJSONFile encodes the given structure into JSON format and writes it to the
 // file on a given path.
 func WriteJSONFile(path string, obj any) error {
